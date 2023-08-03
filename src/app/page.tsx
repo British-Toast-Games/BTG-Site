9380924cd876e4ed5ca798d2da20d65b1e3a9713
Noah Bedouch <noahbedouch@gmail.com>
2023-08-03 15:04:19 -0700

    Fix gamecard spacing and sizing

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -11,11 +11,14 @@ export default function Home() {
           <h1 className="text-midnight-100 mx-auto font-IBM font-bold pt-80 pb-60 md:text-6xl text-4xl">Glorsh globanab glinkport glibbybop gleebenop.</h1>
         </div>
       </div>
-      <div id="Games" className="bg-midnight-950 mx-auto">
+      <div id="Games" className="bg-midnight-950">
         <h1 className="text-midnight-100 mx-auto font-IBM font-bold text-5xl py-20 text-center">We make games an allat</h1>
-        <div className="flex mx-auto justify-center px-20 py-5">
-          <Gamecard title="Two Words: I'm Burger" imageSource={'/TWIBImage.png'} description="What would you do if a fast food employee stole your credit card. Well, in Two Words: I'm Burger, Fregley takes action." alt="Cover art for Two Words: I'm Burger"></Gamecard>
+        <div className="flex flex-wrap mx-auto w-fit pb-20">
+          <Gamecard title="Two Words: I'm Burger" imageSource={'/TWIBImage.png'} description="What would you do if a fast food employee stole your credit card? Well, in Two Words: I'm Burger, Fregley is faced with this exact scenario and he takes action." alt="Cover art for Two Words: I'm Burger"/>
         </div>
+      </div>
+      <div className="bg-midnight-900">
+        <h1 className="text-midnight-100 mx-auto font-IBM font-bold text-5xl py-20 text-center">About Us</h1>
       </div>
     </div>
   )
--- src/components/Cards/gamecard.tsx
+++ src/components/Cards/gamecard.tsx
@@ -1,13 +1,11 @@
 import Image from "next/image"
 
-export default function Gamecard(props: {title: string, imageSource: string, description: string, alt: string}) {
+export default function Gamecard(props: {title: string, imageSource: string, description: string, alt: string, className?: string}) {
     return (
-        <div>
-            <div className="p-2 hover:border rounded-md border-midnight-100 container">
-                <Image src={props.imageSource} alt={props.alt} width={500} height={500} className="rounded-md"/>
-                <h1 className="text-midnight-100 font-IBM font-bold text-center text-2xl pt-4">{props.title}</h1>
-                <p className="text-midnight-100 font-IBM pt-2">{props.description}</p>
-            </div>
+        <div className={`w-96 bg-midnight-800 p-6 hover:border hover:p-[calc(1.5rem-1px)] rounded-md border-midnight-100 ${props.className}`}>
+            <Image src={props.imageSource} alt={props.alt} width={500} height={500} className="rounded-md"/>
+            <h1 className="text-midnight-100 font-IBM font-bold text-center text-2xl pt-4">{props.title}</h1>
+            <p className="text-midnight-100 font-IBM pt-2">{props.description}</p>
         </div>
     )
 }
